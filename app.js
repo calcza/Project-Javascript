171a129dc16b6338a353dd7d688d7a25fd00d549
calcza <32948907+calcza@users.noreply.github.com>
2018-02-21 11:25:05 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -21,7 +21,11 @@ init();
 
 var a = 1;
 var y = 1;
+var z = 1;
+var x = 1;
 var diceNo = []
+var combo = 10;
+var combo2 = 7;
 
 function init() {
     scores = [0,0];
@@ -29,10 +33,18 @@ function init() {
     activePlayer = 0;
     gamePlaying = true;
     goalSet = 100;
-    dicevalue = 1; //how many dice
+    dicevalue = 4; //how many dice
+    document.getElementById("panel").value = 1;
     diceNo = [] //if more than one dice will be put into array
+    a = 1;
+    y = 1;
+    z = 1;
+    x = 1;
+    combo = 10;
+    combo2 = 7;
 
     document.querySelector('.dice').style.display = 'none';
+    diceDisplay();
 
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
@@ -66,8 +78,10 @@ function init() {
 document.querySelector('.btn-roll').addEventListener('click', function() {
     if(gamePlaying) {
         if ( dicevalue > 1 ){
-            diceRule();
+            diceRolls();
         } else {
+            document.querySelector('.dice').style.left = "50%";
+            document.querySelector('.dice').style.top = "220px";
             //random number
             dice = Math.floor(Math.random() * 6) + 1;
             // display the result
@@ -153,18 +167,14 @@ document.querySelector('.slider').addEventListener('mousemove', function(){
     var defaultElem = elem.defaultValue;
     var currentElem = elem.value;
     dicevalue = currentElem;
+    diceDisplay();
     if (diceNo.length > dicevalue) {
         var minus = diceNo.length - dicevalue;
         for (var i = 0; i <= minus; i++) {
             diceNo.pop();
         }
     }
-    // if (defaultElem < currentElem){
-    //     function(currentElem) {
-    //     document.querySelector('.dice').style.display = 'none';
-    //
-    //     }
-    // }
+
 });
 
 function nextPlayer() {
@@ -193,81 +203,121 @@ function nextPlayer() {
     document.querySelector('.wrapper').classList.remove('gameOver');
 
     document.querySelector('.dice').style.display = 'none';
+    diceDisplay();
 
 }
 
-function diceRule() {
-    var OneLoop = true;
+function diceRolls() {
+    combo = dicevalue * 6;//ensure no same amount ever created with combo and combo2
+    combo2 = dicevalue * 5; //ensure no same amount ever created with combo and combo2
+    a = 1;
+    y = 1;
+    z = 1; //counter for a + y
+    x = 1;
     for (var i = 0; i <= dicevalue - 1; ++i) {
         diceNo[i] = Math.floor(Math.random() * 6) + 1;
         console.log("Total dice: " + dicevalue + " dice no. " + i + " rolled: "+ diceNo[i])
-        if (diceNo.length <= 2 && OneLoop == true) {
-            if (diceNo[i] == 1 || diceNo[i] == 6) {
-                if (diceNo[i] == 1) {
-                    a++;
-                    console.log("A value: " + a)
-                } else if (diceNo[i] === 6) {
-                    y++;
-                    console.log("Y value: " + y)
-                }
+        var order = 'dicey' + i;
+        var diceDis = []
+        diceDis[i] = document.getElementById(order);
+        diceDis[i].style.display = 'block';
+        if ( dicevalue > 1 && i % 2 == 0){
+            document.querySelector('.dice').style.left = "30%";
+            console.log(document.querySelector('.dice').style.left);
+            console.log(document.querySelector('.dice').style.top);
+            console.log("Dice No: " + i);
+            if (i == 2) {
+                document.querySelector('.dice').style.left = "50%";
+                document.querySelector('.dice').style.top = "240px";
+                console.log(document.querySelector('.dice').style.left);
+                console.log(document.querySelector('.dice').style.top);
+                console.log("Dice No: " + i);
             }
-            if ( a != 3 || y != 3 || a != 2, y != 2 ) {
-                roundScore += diceNo[i];
-                document.querySelector('#current-' + activePlayer).textContent = roundScore;
-                console.log("Score: " + roundScore)
-            } else {
-                console.log("A value: " + a)
-                console.log("Y value: " + y)
-                a = 1;
-                y = 1;
-                OneLoop = false;
-                setTimeout(nextPlayer, 0000);
-                console.log("player " + activePlayer);
-                console.log("nextPlayer")
+
+        } else if (dicevalue > 1 && i % 2 == 1) {
+            document.querySelector('.dice').style.left = "70%";
+            document.querySelector('.dice').style.top = "220px";
+            console.log(document.querySelector('.dice').style.left);
+            console.log(document.querySelector('.dice').style.top);
+            console.log("Dice No: " + i);
+            if ( i == 3 ){
+                document.querySelector('.dice').style.left = "80%";
+                document.querySelector('.dice').style.top = "240px";
+                console.log(document.querySelector('.dice').style.left);
+                console.log(document.querySelector('.dice').style.top);
+                console.log("Dice No: " + i);
             }
-        } else {
-            console.log("A value: " + a)
-            console.log("Y value: " + y)
-            a = 1;
-            y = 1;
-            OneLoop = false;
-            setTimeout(nextPlayer, 0000);
-            console.log("player " + activePlayer);
-            console.log("nextPlayer")
         }
+        diceDis[i].src = 'dice-' + diceNo[i] + '.png';
+        checkDice(i);
     }
 }
 
 document.querySelector('.btn-new').addEventListener('click', init);
 
-function diceCheck(){
-
-}
 
 function gameOver(){
     document.querySelector('.wrapper').classList.add('gameOver');
     document.querySelector('.active').style.backgroundColor = 'rgba(255,0,0,0.3)';
     document.querySelector('.btn-roll').disabled = true;
     setTimeout(nextPlayer, 1000);
-
 }
 
-// if (dicevalue = 1) {
-//     //update the round score IF the rolled number was NOt a 1
-//     if (dice !== 1 ) {
-//         roundScore  += dice;
-//         document.querySelector('#current-' + activePlayer).textContent = roundScore;
-//         if(dice === 6){
-//             a++;
-//             if (a === 3) {
-//                 setTimeout(nextPlayer, 0000);
-//                 a = 1;
-//             }
-//         } else {
-//             a = 1;
-//         }
-//     } else {
-//         setTimeout(nextPlayer, 0000);
-//         a = 1;
-//     }
-// }
+function diceDisplay(){
+    document.querySelector('.dice').style.display = 'block';
+    for (var i = 0; i <= 4; ++i){
+        var order = 'dicey' + i;
+        var diceDom = document.getElementById(order);
+        diceDom.style.display = 'none';
+    }
+}
+
+function checkDice(i){
+    var OneLoop = true; //protecting against multiple loop
+    var diceOrder = [] //order of the dice to use with combo, combo2
+    console.log("Combo Even: " + combo2);
+    console.log("Combo Odd: " + combo);
+    if (diceNo[i] == 1 || diceNo[i] == 6) {
+        if (diceNo[i] == 1) {
+            a++;
+            z++;
+            console.log("A value: " + a)
+        } else if (diceNo[i] === 6) {
+            y++;
+            z++;
+            console.log("Y value: " + y)
+        }
+    // }
+    } else if (i % 2 == 0 && combo2 == diceNo[i] && dicevalue > 2 && i !== 0) {
+        x++; //check for same value for odd dices e.g. dice 1 and dice 3 is the same value or not
+    } else if (i % 2 == 1 && combo == diceNo[i] && dicevalue > 2 && i !== 1) {
+        x++; //check for same value for even dices (incase for 4 or more dices)
+    } else if (i % 2 == 1 && combo2 == diceNo[i] && dicevalue == 2) {
+        x++; //for two dices play
+    }
+    if (dicevalue % 2 == 0 && OneLoop == true) {
+        var b = [3,3,3,2] //Counter against var a,y,z,x
+        if (dicevalue > 2) {
+            b = [4,4,4,3]
+        }
+        if ( a <= b[0] && y <= b[1] && z <= b[2] && x != b[3] && (x + z !== 5)) {
+            roundScore += diceNo[i];
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        } else {
+            console.log("A value: " + a);
+            console.log("Y value: " + y);
+            console.log("Z value: " + z);
+            console.log("X value: " + x);
+            OneLoop = false; //protecting against multiple loop
+            gameOver();
+        }
+    } else {
+        OneLoop = false;
+        gameOver();
+    }
+    if ( i % 2 == 0){
+        combo2 = diceNo[i]; //store even dice
+    } else if (i % 2 == 1){
+        combo = diceNo[i]; //store odd dice
+    }
+}
